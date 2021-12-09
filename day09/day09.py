b970233684f0b2007176df95c110150b037513d1
Karamba91 <erikhenriksson91@gmail.com>
2021-12-09 20:13:33 +0100

    Make solution for part 1 more compact

--- day09/day09.py
+++ day09/day09.py
@@ -2,32 +2,10 @@ def low_point_sum(in_map):
     low_points = []
     for y, row in enumerate(in_map):
         for x, point in enumerate(row):
-            if (y == 0) and (x==0):
-                if in_map[y+1][x] > point and row[x+1] > point:
-                    low_points += [point]
-            elif (y == 0) and (x==len(row)-1):
-                if in_map[y+1][x] > point and row[x-1] > point:
-                    low_points += [point]
-            elif (y == len(in_map)-1) and (x == 0):
-                if in_map[y-1][x] > point and row[x+1] > point:
-                    low_points += [point]
-            elif (y == len(in_map)-1) and (x==len(row)-1):
-                if in_map[y-1][x] > point and row[x-1] > point:
-                    low_points += [point]
-            elif (y == 0):
-                if all(map(lambda x: x > point, [in_map[y+1][x], row[x-1], row[x+1]])):
-                    low_points += [point]
-            elif (x == 0):
-                if all(map(lambda x: x > point, [in_map[y+1][x], in_map[y-1][x], row[x+1]])):
-                    low_points += [point]
-            elif (x == len(row)-1):
-                if all(map(lambda x: x > point, [in_map[y+1][x], in_map[y-1][x], row[x-1]])):
-                    low_points += [point]
-            elif (y == len(in_map)-1):
-                if all(map(lambda x: x > point, [in_map[y-1][x], row[x-1], row[x+1]])):
-                    low_points += [point]
-            elif all(map(lambda x: x > point, [in_map[y-1][x], in_map[y+1][x], row[x-1], row[x+1]])):
-                low_points += [point]
+            new_coord = [(x-1, y), (x+1, y), (x, y-1), (x, y+1)]
+            new_f_coord = [c for c in new_coord if not not_valid(c, len(row)-1, len(in_map)-1)]
+            if all([point < in_map[x[1]][x[0]] for x in new_f_coord]):
+                low_points += [point]        
     return low_points
 
 def find_basins(in_map):
@@ -35,10 +13,8 @@ def find_basins(in_map):
     basins = list()
     for y, row in enumerate(in_map):
         for x, point in enumerate(row):
-            if point == '9':
+            if point == '9' or ((x,y) in visited):
                 visited.update((x,y))
-                continue
-            elif (x,y) in visited:
                 continue
             unexplored = set([(x,y)])
             basin = set()
@@ -50,7 +26,8 @@ def find_basins(in_map):
                 new_coord = [(check_coord[0]-1, check_coord[1]), (check_coord[0]+1, check_coord[1]),
                                 (check_coord[0], check_coord[1]-1), (check_coord[0], check_coord[1]+1)]
                 unexplored.update([coord for coord in new_coord 
-                if (coord not in visited) and (coord not in basin) and (not not_valid(coord, len(row)-1, len(in_map)-1))])
+                                    if (coord not in visited) and (coord not in basin)
+                                    and (not not_valid(coord, len(row)-1, len(in_map)-1))])
                 visited.update([check_coord])
                 basin.update([check_coord])
             basins += [basin]
@@ -64,10 +41,8 @@ if __name__=='__main__':
     with open("day09/input.txt") as f:
         input_lst = list(map(str.strip, f.readlines()))
     result = low_point_sum(input_lst)
-    print(result)
-    print(sum(map(lambda x: int(x)+1, result)))
+    print("Sum of risk at low points: %d" % sum(map(lambda x: int(x)+1, result)))
     basins = find_basins(input_lst)
     basins = list(map(len, basins))
     basins.sort()
-    print("Sizes of basins " + str(basins)) 
     print("Multiplying the largest gives: %d" % (basins[-1]*basins[-2]*basins[-3]))
